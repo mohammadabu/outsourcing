098635200197a63274b85c30e471d060a67866a6
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-01-18 14:01:26 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -21,8 +21,8 @@
     ],
     'description': "",
     'data': [
-        # 'security/outsourcing_security.xml',
-        # 'security/ir.model.access.csv',
+        'security/outsourcing_security.xml',
+        'security/ir.model.access.csv',
         # 'report/outsourcing_report_views.xml',
         # 'views/analytic_views.xml',
         # 'views/digest_views.xml',
